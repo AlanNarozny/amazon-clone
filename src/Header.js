0ea8c8861418370cd99c0ab6ba330f4f209787f4
Alan N <alan.narozny@hotmail.com>
2020-09-09 15:31:26 +0200

    styling header with material UI

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 function Header() {
   return (
@@ -11,7 +13,7 @@ function Header() {
 
       <div className="header__search">
         <input type="text" className="header__searchInput" />
-        {/*logo*/}
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
@@ -28,6 +30,11 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingCartIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
