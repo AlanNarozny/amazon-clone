aab3cf748baa8398b98af66fa7d8e88e037f8e30
Alan N <alan.narozny@hotmail.com>
2020-09-09 14:51:21 +0200

    style main header, install ,eterial ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     //BEM convention styling - lowercases
     <div className="app">
-      <h1>Hello Clever Programmers, lets build the Amazon Store</h1>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
