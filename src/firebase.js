00ff1335bf4d85c1809b48739a4cabdaab6780b4
Alan N <alan.narozny@hotmail.com>
2020-09-09 14:28:13 +0200

    add header toolbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcmA6BRFViwbX6cWQOkpgRAev1JoGPEfA",
+    authDomain: "challenge-b6e78.firebaseapp.com",
+    databaseURL: "https://challenge-b6e78.firebaseio.com",
+    projectId: "challenge-b6e78",
+    storageBucket: "challenge-b6e78.appspot.com",
+    messagingSenderId: "1083558842542",
+    appId: "1:1083558842542:web:3b371b2f8bdce3b113dd3f",
+    measurementId: "G-VDTJDD3DDC"
+  };
