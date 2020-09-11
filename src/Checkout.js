2f329359166c9792f423d52f54991b139d5e3879
Alan N <alan.narozny@hotmail.com>
2020-09-11 13:54:34 +0200

    add remove form basket functionality

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,8 +2,11 @@ import React from "react";
 import "./Checkout.css";
 import { Link } from "@material-ui/core";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -17,8 +20,15 @@ function Checkout() {
           <h2>
             <h2 className="checkout__title">Your shopping basket</h2>
           </h2>
-
-          {/* Basket Item*/}
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
 
