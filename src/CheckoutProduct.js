7e3aa61ee1b038024d3a61de7aad0907bf366da0
Alan N <alan.narozny@hotmail.com>
2020-09-29 12:04:35 +0200

    add Order page and styling, refactor Orders and Payment

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -3,8 +3,7 @@ import "./CheckoutProduct.css";
 import StarIcon from "@material-ui/icons/Star";
 import { useStateValue } from "./StateProvider";
 
-function CheckoutProduct({ id, image, title, price, rating }) {
-  
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
   const [{ basket }, dispatch] = useStateValue();
 
   const removeFromBasket = () => {
@@ -31,7 +30,9 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <StarIcon key={index} />
             ))}
         </div>
-        <button onClick={removeFromBasket}>Remove from basket</button>
+        {!hideButton && (
+          <button onClick={removeFromBasket}>Remove from basket</button>
+        )}
       </div>
     </div>
   );
